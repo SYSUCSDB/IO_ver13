3dae04cd9a372653c73f3cd5e7e65dd15080b37c
Cudd1es <32083795+Cudd1es@users.noreply.github.com>
2017-12-20 02:06:48 +0800

    Android Studio中间崩了一次
    好伤

--- app/src/main/java/com/a/io_ver13/CreateActivity.java
+++ app/src/main/java/com/a/io_ver13/CreateActivity.java
@@ -1,7 +1,10 @@
 package com.a.io_ver13;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by pengb on 2017/12/19.
@@ -11,5 +14,19 @@ public class CreateActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_page);
+        //add button event here
+        Button button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*link create page here*/
+                Intent intent = new Intent();
+                intent.setClass(CreateActivity.this, DateSelecter.class);
+                startActivity(intent);
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+            }
+        });
     }
+
 }
