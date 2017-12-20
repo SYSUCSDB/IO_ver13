abedc52c87870dd9fc9b56e01c6442517c05759f
Cudd1es <32083795+Cudd1es@users.noreply.github.com>
2017-12-20 19:07:29 +0800

    Added date selection and time selection on "ADD" page

--- app/src/main/java/com/a/io_ver13/CreateActivity.java
+++ app/src/main/java/com/a/io_ver13/CreateActivity.java
@@ -3,11 +3,13 @@ package com.a.io_ver13;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 /**
  * Created by pengb on 2017/12/19.
@@ -18,8 +20,8 @@ public class CreateActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_page);
         //add button event here
-        Button button = (Button)findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button button_date = (Button)findViewById(R.id.button_date_selecter);
+        button_date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 /*link create page here*/
@@ -31,11 +33,21 @@ public class CreateActivity extends Activity {
                         .setAction("Action", null).show();*/
             }
         });
-    }
+        Button button_time = (Button)findViewById(R.id.button_time_selecter);
+        button_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                showDialog(2);
+            }
+        });
+        };
+
     protected Dialog onCreateDialog(int id) {
         switch (id) {
             case 1:
                 return new DatePickerDialog(this, mdateListener, 2017, 12-1, 20);
+            case 2:
+                return new TimePickerDialog(this, mtimeListener, 18, 58, true);
         }
         return null;
     }
@@ -47,8 +59,16 @@ public class CreateActivity extends Activity {
             /*mYear = year;
             mMonth = monthOfYear;
             mDay = dayOfMonth;*/
-            Button button1 = (Button)findViewById(R.id.button_date_selecter);
-            button1.setText(new StringBuffer().append(year).append("-").append(monthOfYear+1).append("-").append(dayOfMonth));
+            Button button_date = (Button)findViewById(R.id.button_date_selecter);
+            button_date.setText(new StringBuffer().append(year).append("-").append(monthOfYear+1).append("-").append(dayOfMonth));
+        }
+    };
+    private TimePickerDialog.OnTimeSetListener mtimeListener = new TimePickerDialog.OnTimeSetListener() {
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            Button button_time = (Button)findViewById(R.id.button_time_selecter);
+            button_time.setText(new StringBuffer().append(hourOfDay).append(":").append(minute));
         }
     };
 }
