fd758b65234f2199c741cc883bb6ed779bd62982
pengbd <961862555@qq.com>
2017-12-20 18:17:13 +0800

    set the date picker outside

--- app/src/main/java/com/a/io_ver13/CreateActivity.java
+++ app/src/main/java/com/a/io_ver13/CreateActivity.java
@@ -1,10 +1,13 @@
 package com.a.io_ver13;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 /**
  * Created by pengb on 2017/12/19.
@@ -20,13 +23,30 @@ public class CreateActivity extends Activity {
             @Override
             public void onClick(View view) {
                 /*link create page here*/
-                Intent intent = new Intent();
+                /*Intent intent = new Intent();
                 intent.setClass(CreateActivity.this, DateSelecter.class);
-                startActivity(intent);
+                startActivity(intent);*/
+                showDialog(1);
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
             }
         });
     }
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case 1:
+                return new DatePickerDialog(this, mdateListener, 2017, 12-1, 20);
+        }
+        return null;
+    }
+    private DatePickerDialog.OnDateSetListener mdateListener = new DatePickerDialog.OnDateSetListener() {
 
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            /*mYear = year;
+            mMonth = monthOfYear;
+            mDay = dayOfMonth;*/
+        }
+    };
 }
