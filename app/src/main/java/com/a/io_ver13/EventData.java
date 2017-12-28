ea6b3b2f90365e0ebe1f5979ca8882faabd6a858
Quyi5 <quyi5@mail2.sysu.edu.cn>
2017-12-29 00:20:40 +0800

    上周我们的成品
    最终版，请所有人在此版本上更新

---
+++ app/src/main/java/com/a/io_ver13/EventData.java
@@ -0,0 +1,17 @@
+package com.a.io_ver13;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2017/12/27.
+ */
+
+public class EventData {
+    public int event_id;
+    public String event_title;
+    public Date event_date;
+    public Time event_time;
+    public boolean event_if_alarm;
+    public String event_note;
+}
