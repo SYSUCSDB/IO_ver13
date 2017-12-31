d88bd7b507cfc95e0e1f7191189283a41a46e0f8
wuxm23 <1357261750@qq.com>
2017-12-31 18:08:22 +0800

    card+DB
    从DB读取数据并显示，长按删除卡片和数据库内容，用数组测试成功，createPage文件里有一个set_enable与EventData冲突了，我就把它注释掉了。

--- app/src/main/java/com/a/io_ver13/CreateActivity.java
+++ app/src/main/java/com/a/io_ver13/CreateActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -19,10 +20,16 @@ import android.widget.TimePicker;
  */
 
 public class CreateActivity extends Activity {
+    public final int CREATE_REQUEST = 1;
+    public final int CREATE_RESULT = 2;
+    public final int DATE_DIALOG = 1;
+    public final int TIME_DIALOG = 2;
+    public EventData m_event_data;
     public boolean i = true;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_page);
+        m_event_data = new EventData();
         //add button event here
         Button button_date = (Button)findViewById(R.id.button_date_selecter);
         button_date.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +39,7 @@ public class CreateActivity extends Activity {
                 /*Intent intent = new Intent();
                 intent.setClass(CreateActivity.this, DateSelecter.class);
                 startActivity(intent);*/
-                showDialog(1);
+                showDialog(DATE_DIALOG);
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
             }
@@ -41,7 +48,7 @@ public class CreateActivity extends Activity {
         button_time.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                showDialog(2);
+                showDialog(TIME_DIALOG);
             }
         });
         Button button_confirm = (Button)findViewById(R.id.button_confirm);
@@ -57,12 +64,16 @@ public class CreateActivity extends Activity {
                 String m_event_note = "";
                 m_event_note += edit_note.getText().toString().trim();
                 Intent return_intent = new Intent();
-                return_intent.putExtra("event_title", m_event_title);
-                return_intent.putExtra("event_note", m_event_note);
-                //finish
-
+                //return_intent.putExtra("event_title", m_event_title);
+                //return_intent.putExtra("event_note", m_event_note);
                 //return_intent.putExtra("i", i);
-                setResult(2, return_intent);
+                //finish
+               // m_event_data.set_enable();
+                m_event_data.set_event_title(m_event_title);
+                m_event_data.set_event_note(m_event_note);
+                //DBOper m_dboper = new DBOper(CreateActivity.this);
+                //m_dboper.insert(m_event_data);
+                setResult(CREATE_RESULT, return_intent);
                 finish();
             }
         });
@@ -85,9 +96,9 @@ public class CreateActivity extends Activity {
 
     protected Dialog onCreateDialog(int id) {
         switch (id) {
-            case 1:
+            case DATE_DIALOG:
                 return new DatePickerDialog(this, mdateListener, 2017, 1-1, 1);
-            case 2:
+            case TIME_DIALOG:
                 return new TimePickerDialog(this, mtimeListener, 1, 1, true);
         }
         return null;
@@ -102,6 +113,7 @@ public class CreateActivity extends Activity {
             mDay = dayOfMonth;*/
             Button button_date = (Button)findViewById(R.id.button_date_selecter);
             button_date.setText(new StringBuffer().append(year).append("-").append(monthOfYear+1).append("-").append(dayOfMonth));
+            m_event_data.set_event_day(year, monthOfYear, dayOfMonth);
         }
     };
     private TimePickerDialog.OnTimeSetListener mtimeListener = new TimePickerDialog.OnTimeSetListener() {
@@ -110,6 +122,7 @@ public class CreateActivity extends Activity {
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             Button button_time = (Button)findViewById(R.id.button_time_selecter);
             button_time.setText(new StringBuffer().append(hourOfDay).append(":").append(minute));
+            m_event_data.set_event_time(hourOfDay, minute, 0);
         }
     };
 }
--- app/src/main/java/com/a/io_ver13/MainActivity.java
+++ app/src/main/java/com/a/io_ver13/MainActivity.java
@@ -1,6 +1,8 @@
 package com.a.io_ver13;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,14 +11,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.ParseException;
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import static com.a.io_ver13.R.id.action_settings;
+
+
+public class MainActivity extends AppCompatActivity{
     //initialize data here
+    public final int CREATE_REQUEST = 1;
+    public final int CREATE_RESULT = 2;
     public int event_id;
     public String event_title;
     public Date event_date;
@@ -25,48 +41,159 @@ public class MainActivity extends AppCompatActivity {
     public String event_note;
     public boolean i = false;
 
+    public EventData a;
+
+    private ListView listView;
+    private DBOper dbOper;
+    private ArrayList<EventData> eventlist;
+    private  SimpleAdapter adapter;
+
+    private static final String[] title = {"事件1", "事件2", "事件3", "事件4", "事件5", "事件6", "事件7", "事件8", "事件9", "事件10"};
+    private static final String[] date = {"2017-12-30 23:59:59", "2017-12-30 23:59:59", "2017-12-30 23:59:59", "2017-12-30 23:59:59", "2017-12-30 23:59:59", "2017-12-30 23:59:59", "2017-12-30 23:59:59", "2017-12-30 23:59:59", "2017-12-30 23:59:59", "2017-12-30 23:59:59"};
+    private static final String[] note = {"1111", "1111", "1111", "1111", "1111", "1111", "1111", "1111", "1111", "1111"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        listView = (ListView) findViewById(R.id.card_listView);
+        a = new EventData();
+        initView();
+        dbOper = new DBOper(this);
+        try {
+            eventlist = dbOper.query();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+       final List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+        for(int i = 0; i < /*eventlist.size()*/title.length; i++) {
+            HashMap<String, Object> item = new HashMap<String, Object>();
+            //数据库读取
+            /*EventData temp = eventlist.get(i);
+            item.put("data1", temp.get_event_title());
+            item.put("data2", temp.get_event_data());
+            item.put("data3", temp.get_event_note());
+            item.put("data4", R.mipmap.tu);*/
+            //数组测试
+            item.put("data1", title[i]);
+            item.put("data2", date[i]);
+            item.put("data3", note[i]);
+            item.put("data4", getResource(i));
+            list.add(item);
+        }
+        adapter = new SimpleAdapter(this, list, R.layout.list_item_card, new String[]{"data1", "data2", "data3", "data4"}, new int[]{R.id.title, R.id.date, R.id.note, R.id.image});
+        //添加头和尾
+        listView.addHeaderView(new View(this));
+        listView.addFooterView(new View(this));
+        listView.setAdapter(adapter);
+
+        //删除
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //数据库删除
+                /*dbOper.delete(eventlist.get(i));
+                try {
+                    eventlist = dbOper.query();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }*/
+                //数组删除
+                list.remove(i-1);
+                ((BaseAdapter)adapter).notifyDataSetChanged();
+                return true;
+            }
+        });
+
+        FloatingActionButton fab;
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 /*link create page here*/
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this, CreateActivity.class);
-                startActivityForResult(intent, 1);
+                startActivityForResult(intent, CREATE_REQUEST);
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
+            }
+        });
+        Toolbar menu = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(menu);
+
+        menu.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_settings:
+                        //Toast.makeText(MainActivity.this, "yes!", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent();
+                        intent.setClass(MainActivity.this, Settings.class);
+                        startActivity(intent);
+                        break;
+
+                    default:
+                        break;
+                }
+                return true;
             }
         });
 
     }
 
+    private int getResource(int index) {
+        int res;
+        //if(index%2 == 0) {
+            res = R.mipmap.tu;
+       /* }
+        else {
+            res = R.mipmap.icon;
+        }*/
+        return res;
+    };
+
+    private void initView() {
+        listView = (ListView)findViewById(R.id.card_listView);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent return_intent) {
         super.onActivityResult(requestCode, resultCode, return_intent);
-        if(resultCode == 2) {
-            if(requestCode == 1) {
-                event_title = return_intent.getStringExtra("m_event_title");
-                event_note = return_intent.getStringExtra("m_event_note");
-                i = return_intent.getBooleanExtra("i",false);
+        if(resultCode == CREATE_RESULT) {
+            if(requestCode == CREATE_REQUEST) {
+                //event_title = return_intent.getStringExtra("m_event_title");
+                //event_note = return_intent.getStringExtra("m_event_note");
+                //i = return_intent.getBooleanExtra("i",false);
                 TextView show_title = (TextView)findViewById(R.id.show_title);
-                show_title.setText(event_title);
+                //show_title.setText(event_title);
                 TextView show_note = (TextView)findViewById(R.id.show_note);
-                show_note.setText(event_note);
+                //show_note.setText(event_note);
                 /*Button btn_confirmed = (Button)findViewById(R.id.button_confirm);
                 if(i)
                     btn_confirmed.setText("yes");
                 else
                     btn_confirmed.setText("nooooooo!");*/
+                //DBOper oper = new DBOper(this);
+                //Cursor c = oper.queryCursor();
+                //在这个地方把a里的数据赋成数据库里读到的，可以用那两行字测试
+                a.set_event_title("title");
+                a.set_event_note("note");
+                show_title.setText(a.get_event_title());
+                show_note.setText(a.get_event_note());
             }
         }
+
+        //点击确认后执行以下更新操作：
+        //重新查询数据库
+        try {
+            eventlist = dbOper.query();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        //刷新数据适配器
+        adapter.notifyDataSetChanged();
+
     }
 
     @Override
@@ -84,7 +211,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == action_settings) {
             return true;
         }
 
